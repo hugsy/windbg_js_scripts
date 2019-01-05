f94e66281161caa4b6c2c9e8cde630d18f8f6d44
chris <christophe.alladoum@sophos.com>
2019-01-04 17:55:28 -0800

    minor change

--- JsSkeleton.js
+++ JsSkeleton.js
@@ -12,7 +12,7 @@ const u16 = x => host.memory.readMemoryValues(x, 1, 2)[0];
 const u32 = x => host.memory.readMemoryValues(x, 1, 4)[0];
 const u64 = x => host.memory.readMemoryValues(x, 1, 8)[0];
 
-function IsX64(){return host.namespace.Debugger.State.PseudoRegisters.General.ptrsize == 8;}	const PointerSize = CurrentSession.Attributes.Machine.PointerSize;
+function IsX64(){return host.namespace.Debugger.State.PseudoRegisters.General.ptrsize == 8;}
 function IsKd() { return host.namespace.Debugger.Sessions.First().Attributes.Target.IsKernelTarget === true; }
 function $(r){ if(!IsKd()) return host.currentThread.Registers.User[r]; else return host.namespace.Debugger.State.DebuggerVariables.curprocess.Threads.First().Registers.User[r] || host.namespace.Debugger.State.DebuggerVariables.curprocess.Threads.First().Registers.Kernel[r]; }
 function GetSymbolFromAddress(x){ return system('.printf "%y", ' + x.toString(16)).First(); }
