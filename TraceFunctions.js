dc3fb19a1edc99af580b300deea6920e1cd8048d
chris <christophe.alladoum@sophos.com>
2019-05-16 16:19:19 -0700

    [tracefunctions] added x86 supports, and removed the 4 arg limit

--- TraceFunctions.js
+++ TraceFunctions.js
@@ -5,37 +5,81 @@
  * Use with
  * 0:000> .scriptload \path\to\TraceFunctions.js
  * 0:000> !trace "ntdll!memset"
+ * Or
+ * 0:000> !trace "ntdll!memset", 3
+ * Or
+ * 0:000> !trace "8509f008", 2
+ * Or
+ * 0:000> !trace "ntdll+0x245f"
  */
 
 "use strict";
 
 const log = x => host.diagnostics.debugLog(x + "\n");
 const system = x => host.namespace.Debugger.Utility.Control.ExecuteCommand(x);
+const u32 = x => host.memory.readMemoryValues(x, 1, 4)[0];
+const u64 = x => host.memory.readMemoryValues(x, 1, 8)[0];
 
+function IsX64() {return host.namespace.Debugger.State.PseudoRegisters.General.ptrsize === 8;}
 function IsKd() { return host.namespace.Debugger.Sessions.First().Attributes.Target.IsKernelTarget != 0; }
 function $(r){ if(!IsKd()) return host.currentThread.Registers.User[r]; else return host.namespace.Debugger.State.DebuggerVariables.curprocess.Threads.First().Registers.User[r]; }
 
 
+
+
 /**
- * Print the rcx, rdx, r8, r9 registers when called.
- * This function will obviously only work for x64. It can adjusted to x86 by taking
- * the arguments from host.currentThread.Registers.User.rsp.
+ * Print the arguments when called (x64 version)
  *
- * @param {*} sym
+ * @param {*} loc
+ * @param {*} n
+ * @param {*} comment
  */
-function PrintRegistersCallback(sym)
+function PrintRegistersCallback64(loc, n, comment="")
 {
+    let ptrsize = host.namespace.Debugger.State.PseudoRegisters.General.ptrsize;
     let regs = ["rcx", "rdx", "r8", "r9"];
-    let i = 0;
-    let output = sym;
-    output += "(";
-    for( let i of [...Array(4).keys()] )
+    let output = new Array();
+
+    for( let i of [...Array( Math.min(n, 4) ).keys()] )
     {
-        output += "arg[" + i.toString() + "]=" + $(regs[i]).toString(16) + " ";
+        output.push(`arg[${i.toString()}]=${$(regs[i]).toString(16)}`);
     }
-    output += ")";
-    log(output);
+
+    if(n > 4)
+    {
+        for( let i of [...Array(n - 4).keys()] )
+        {
+            let index = i + 4;
+            let arg = u64($("rsp") + ptrsize*(index + 1));
+            output.push(`arg[${i.toString()}]=${arg.toString(16)}`);
+        }
+    }
+
+    log(`${comment}${loc}(${output.join(", ")})`);
     return false; // change to `true' to block on return
+}
+
+
+/**
+ * Print the arguments when called (x86 version)
+ *
+ * @param {*} loc
+ * @param {*} n
+ * @param {*} comment
+ */
+function PrintRegistersCallback32(loc, n, comment="")
+{
+    let ptrsize = host.namespace.Debugger.State.PseudoRegisters.General.ptrsize;
+    let output = new Array();
+
+    for( let i of [...Array(n).keys()] )
+    {
+        let arg = u32($("esp") + ptrsize*(i + 1));
+        output.push(`arg[${i.toString()}]=${arg.toString(16)}`);
+    }
+
+    log(`${comment}${loc}(${output.join(", ")})`);
+    return false;
 }
 
 
@@ -45,51 +89,88 @@ function PrintRegistersCallback(sym)
  *
  * @param {*} sym
  */
-function GetSymbol(sym)
+function GetAddressFromSymbol(sym)
 {
-    if (sym.indexOf("!") == -1)
+    //if (sym.indexOf("!") === -1)
+    //{
+    //    let default_modules = ["nt", "ntdll", "kernel32", "kernelbase"];
+    //    for (let mod of default_modules)
+    //    {
+    //        var res = host.getModuleSymbolAddress(mod, sym);
+    //        if (res !== undefined)
+    //        {
+    //            return res;
+    //        }
+    //    }
+    //}
+    // let parts = sym.split("!");
+    // return host.getModuleSymbolAddress(parts[0], parts[1]);  // doesn't work all the time, check why (TODO)
+
+    let res = undefined;
+    for (let line of system(`x ${sym}`))
     {
-        let default_modules = ["nt", "ntdll", "kernel32", "kernelbase"];
-        for (let mod of default_modules)
+        if(line.includes(sym))
         {
-            var res = host.getModuleSymbolAddress(mod, sym);
-            if (res != undefined)
-            {
-                return res;
-            }
+            res = host.parseInt64(line.split(" ")[0], 16);
+            break;
         }
     }
-
-    var parts = sym.split("!");
-    return host.getModuleSymbolAddress(parts[0], parts[1]);
+    return res;
 }
 
 
 /**
- * Set a custom breakpoint in WinDbg, allowing any JS function as callback
+ * Set a custom breakpoint in WinDbg with a JS callback that will dump the `nb`
+ * number of arguments.
+ *
+ * The credit for this breakpoint trick goes to by @0vercl0k
  *
  * @param {*} address
- * @param {*} callback
+ * @param {*} nb_arg
+ * @param {*} comment
  */
-function SetBreakpoint(location, callback)
+function PrintCallArguments(location, nb, comment)
 {
-    let target = GetSymbol(location);
-    let address = target.toString(16);
-    // BP trick by @0vercl0k
-    let cmd = 'bp /w "@$scriptContents.PrintRegistersCallback(\\"'+ location +'\\")" 0x' + address;
-    host.namespace.Debugger.Utility.Control.ExecuteCommand(cmd);
-    log('Breakpoint set for "' + location + '" at ' + target.toString(16));
+    if (!IsKd())
+        return;
+
+    if (nb !== undefined)
+        nb = parseInt(nb);
+    else
+        nb = -1;
+
+    if (comment === undefined)
+        comment = "";
+
+    let address = GetAddressFromSymbol(location);
+    if (address === undefined)
+        address = location;
+    else
+        address = `0x${address.toString(16)}`;
+
+    let cmd = IsX64()
+    ? `bp /w "@$scriptContents.PrintRegistersCallback64(\\"${location}\\", ${nb}, \\"${comment}\\")" ${address}`
+    : `bp /w "@$scriptContents.PrintRegistersCallback32(\\"${location}\\", ${nb}, \\"${comment}\\")" ${address}`;
+
+    system(cmd);
+
+    let msg = `Breakpoint set for "${location}" at ${address}`;
+    if (nb >= 0)
+        msg += ` with ${nb} argument(s)`;
+    log(msg)
 }
 
 
 /**
  * main()
  *
- * Sets up a default tracer for VirtualAlloc.
+ * Example: Sets up a default tracer for
+ * VirtualAlloc(LPVOID lpAddress,SIZE_T dwSize,DWORD flAllocationType,DWORD flProtect);
+ *
  */
 function invokeScript()
 {
-    SetBreakpoint('kernelbase!VirtualAlloc');
+    PrintCallArguments('kernelbase!VirtualAlloc', 4);
 }
 
 
@@ -101,11 +182,8 @@ function invokeScript()
  */
 function initializeScript()
 {
-
-    if(IsKd())
-    {
-       log("[+] Installing new command `!trace`...");
-       return [new host.functionAlias(SetBreakpoint, "trace"),];
-    }
-
+    log("[+] Installing new command `!trace`...");
+    return [
+        new host.functionAlias(PrintCallArguments, "trace"),
+    ];
 }
