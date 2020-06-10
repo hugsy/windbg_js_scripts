8e5003e9640c0ece9c79af4fe879fa3c3126c5f4
hugsy <hugsy@blah.cat>
2020-06-09 17:34:52 -0700

    [pageexplorer] added flags parsing & permission

--- scripts/PageExplorer.js
+++ scripts/PageExplorer.js
@@ -8,15 +8,18 @@
 
 /**
  *
- * !pte replacement
+ * !pte replacement - but way slower because there's no primitive for read/write physical memory in JS
+ *
+ * kd> .scriptload \\path\to\PageExplorer.js"
+ * kd> dx @$pte( @rip )
+ * or
+ * kd> dx @$pte(0xFFFFF78000000000)
  *
  * todo:
- * [ ] add permission
- * [ ] add pfn
+ * [ ] add range search
  */
 
-const DEBUG = true;
-
+const DEBUG = false;
 
 const log  = x => host.diagnostics.debugLog(`${x}\n`);
 const dbg  = x => {if( DEBUG )log(`[+] ${x}`);};
@@ -36,8 +39,99 @@ function IsKd() { return host.namespace.Debugger.Sessions.First().Attributes.Tar
 function $(r){ return IsKd() ? host.namespace.Debugger.State.DebuggerVariables.curthread.Registers.User[r] || host.namespace.Debugger.State.DebuggerVariables.curthread.Registers.Kernel[r] : host.namespace.Debugger.State.DebuggerVariables.curthread.Registers.User[r]; }
 
 
-function u32(x, k=false){if(!k) return host.memory.readMemoryValues(x, 1, 4)[0];let cmd = `!dd 0x${x.toString(16)} l1`;let res = system(cmd)[0].split(" ");let p = res[res.length - 1];return i64(`0x${p.replace("`","")}`);}
-function u64(x, k=false){if(!k) return host.memory.readMemoryValues(x, 1, 8)[0];let cmd = `!dq 0x${x.toString(16)} l1`;let res = system(cmd)[0].split(" ");let p = res[res.length - 1];return i64(`0x${p.replace("`","")}`);}
+function u32(x, k=false){if(!k) return host.memory.readMemoryValues(x, 1, 4)[0];let cmd = `!dd 0x${x.toString(16)}`;let res = system(cmd)[0].split(" ").filter(function(v,i,a){return v.length > 0;});return i64(`0x${res[2].replace("`","")}`);}
+function u64(x, k=false){if(!k) return host.memory.readMemoryValues(x, 1, 8)[0];let cmd = `!dq 0x${x.toString(16)}`;let res = system(cmd)[0].split(" ").filter(function(v,i,a){return v.length > 0;});return i64(`0x${res[2].replace("`","")}`);}
+
+
+class PageEntryFlags
+{
+    constructor(flags)
+    {
+        this.__flags = flags;
+        /**
+        kd> dt nt!_MMPTE_HARDWARE
+        +0x000 Valid            : Pos 0, 1 Bit
+        +0x000 Dirty1           : Pos 1, 1 Bit
+        +0x000 Owner            : Pos 2, 1 Bit
+        +0x000 WriteThrough     : Pos 3, 1 Bit
+        +0x000 CacheDisable     : Pos 4, 1 Bit
+        +0x000 Accessed         : Pos 5, 1 Bit
+        +0x000 Dirty            : Pos 6, 1 Bit
+        +0x000 LargePage        : Pos 7, 1 Bit
+        +0x000 Global           : Pos 8, 1 Bit
+        +0x000 CopyOnWrite      : Pos 9, 1 Bit
+        +0x000 Unused           : Pos 10, 1 Bit
+        +0x000 Write            : Pos 11, 1 Bit
+        [...]
+        */
+        this.Present        = this.__flags.bitwiseAnd( 0b000000000001 ) > 0 ? true : false;
+        this.WriteEnabled   = this.__flags.bitwiseAnd( 0b000000000010 ) > 0 ? true : false;
+        this.Owner          = this.__flags.bitwiseAnd( 0b000000000100 ) > 0 ? true : false;
+        this.WriteThrough   = this.__flags.bitwiseAnd( 0b000000001000 ) > 0 ? true : false;
+        this.CacheDisabled  = this.__flags.bitwiseAnd( 0b000000010000 ) > 0 ? true : false;
+        this.Accessed       = this.__flags.bitwiseAnd( 0b000000100000 ) > 0 ? true : false;
+        this.Dirty          = this.__flags.bitwiseAnd( 0b000001000000 ) > 0 ? true : false;
+        this.Global         = this.__flags.bitwiseAnd( 0b000010000000 ) > 0 ? true : false;
+
+        this.NoExecute      = this.__flags.bitwiseAnd( 0x80000000 ) > 0 ? true : false;
+    }
+
+
+    FlagsToString()
+    {
+        let res = [];
+        res.push( (this.Present)?"P":"-" );
+        res.push( (this.WriteEnabled)?"Rw":"Rd" );
+        res.push( (this.Owner)?"K":"U" );
+        res.push( (this.WriteThrough)?"W":"-" );
+        res.push( (this.CacheDisabled)?"C":"-" );
+        res.push( (this.Accessed)?"A":"-" );
+        res.push( (this.Dirty)?"D":"-" );
+        res.push( (this.Global)?"G":"-" );
+        res.push( (this.NoExecute)?"Nx":"eX" );
+        return res.join("")
+    }
+
+
+    toString()
+    {
+        return `Flags=${this.FlagsToString()}`;
+    }
+}
+
+
+class PageGenericEntry
+{
+    constructor(va)
+    {
+        this.__raw_value = va; //.bitwiseShiftLeft(8).bitwiseShiftRight(8);
+        this.flags = new PageEntryFlags( va.bitwiseAnd(0b111111111111) );
+
+        /**
+        kd> dt nt!_MMPTE_HARDWARE
+        [...]
+        +0x000 PageFrameNumber  : Pos 12, 36 Bits
+        +0x000 ReservedForHardware : Pos 48, 4 Bits
+        +0x000 ReservedForSoftware : Pos 52, 4 Bits
+        +0x000 WsleAge          : Pos 56, 4 Bits
+        +0x000 WsleProtection   : Pos 60, 3 Bits
+        */
+       this.pfn = this.__raw_value.bitwiseShiftRight(12).bitwiseAnd(0xFFFFFFFFF);
+       this.physical_page_address = this.pfn.bitwiseShiftLeft(12);
+    }
+}
+
+
+class PageTableEntry extends PageGenericEntry
+{
+   toString() {return `PTE(PA=${hex(this.physical_page_address)}, PFN=${hex(this.pfn)}, ${this.flags})` };
+}
+
+
+class PageDirectoryEntry extends PageGenericEntry
+{
+    toString() {return `PDE(PA=${hex(this.physical_page_address)}, PFN=${hex(this.pfn)}, ${this.flags})`;}
+}
 
 
 
@@ -48,30 +142,24 @@ class PagedVirtualAddress
         this.va = i64(addr);
         this.cr3 = i64(cr3);
 
-        this.pml4_offset = this.va.bitwiseShiftRight(39).bitwiseAnd( 0b111111111);
-        this.pml4 = this.cr3.add(this.pml4_offset.multiply(ptrsize()));
-
-        this.pdpt_offset = this.va.bitwiseShiftRight(30).bitwiseAnd( 0b111111111);
-        this.pdpt_base = u64(this.pml4, true).bitwiseAnd(~0b111111111111);
-        this.pdpt = this.pdpt_base.add(this.pdpt_offset.multiply(ptrsize()));
-
-        this.pd_offset = this.va.bitwiseShiftRight(21).bitwiseAnd(0b111111111);
-        this.pd_base = u64(this.pdpt, true).bitwiseAnd(~0b111111111111);
-        this.pd = this.pd_base.add(this.pd_offset.multiply(ptrsize()));
-
-        this.pt_offset = this.va.bitwiseShiftRight(12).bitwiseAnd(0b111111111);
-        this.pt_base = u64(this.pd, true).bitwiseAnd(~0b111111111111);
-        this.pt = this.pt_base.add(this.pt_offset.multiply(ptrsize()));
-
+        this.pml4e_offset = this.va.bitwiseShiftRight(39).bitwiseAnd(0b111111111);
+        this.pdpe_offset = this.va.bitwiseShiftRight(30).bitwiseAnd(0b111111111);
+        this.pde_offset = this.va.bitwiseShiftRight(21).bitwiseAnd(0b111111111);
+        this.pte_offset = this.va.bitwiseShiftRight(12).bitwiseAnd(0b111111111);
         this.offset = this.va.bitwiseAnd(0b111111111111);
 
-        this.pa = u64(this.pt, true).bitwiseShiftLeft(8).bitwiseShiftRight(8).bitwiseAnd(~0b111111111111).add(this.offset);
+        this.pml4e = new PageDirectoryEntry( u64(this.cr3.add(this.pml4e_offset.multiply(ptrsize())), true) );
+        this.pdpe = new PageDirectoryEntry( u64(this.pml4e.physical_page_address.add(this.pdpe_offset.multiply(ptrsize())), true) );
+        this.pde = new PageDirectoryEntry( u64(this.pdpe.physical_page_address.add(this.pde_offset.multiply(ptrsize())), true) );
+        this.pte = new PageTableEntry( u64(this.pde.physical_page_address.add(this.pte_offset.multiply(ptrsize())), true) );
+
+        this.pa = this.pte.physical_page_address.add(this.offset);
     }
 
 
     toString()
     {
-        return `VA=0x${hex(this.va)} (PML4=0x${hex(this.pml4)} PDPT=0x${hex(this.pdpt)} PD=0x${hex(this.pd)} PT=0x${hex(this.pt)} Offset=0x${hex(this.offset)})`;
+        return `VA=0x${hex(this.va)}, PA=0x${hex(this.pa)}, Offset=0x${hex(this.offset)}`;
     }
 }
 
@@ -120,10 +208,3 @@ function initializeScript()
     ];
 }
 
-
-/**
- *
- */
-function uninitializeScript()
-{
-}
